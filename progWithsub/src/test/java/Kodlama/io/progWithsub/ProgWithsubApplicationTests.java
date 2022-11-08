93ff20c59501810269dc06c78dc2090b3e5f79f5
1bilgisiz <bilgicsaniye66@gmail.com>
2022-11-08 22:32:36 +0300

    Spring boot project created

---
+++ progWithsub/src/test/java/Kodlama/io/progWithsub/ProgWithsubApplicationTests.java
@@ -0,0 +1,13 @@
+package Kodlama.io.progWithsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgWithsubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
